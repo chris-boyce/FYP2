b97fa43bd2ccc17c86990a13a41259c82e1c7573
Christopher Boyce <57403492+chris-boyce@users.noreply.github.com>
2023-03-09 07:24:48 +0000

    Bot Creation

---
+++ Source/FYP5/Public/Bot_AI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Bot_AI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FYP5_API ABot_AI : public AAIController
+{
+	GENERATED_BODY()
+	
+};
