10178eabca8a3522fe94151974831b5c2671eeca
Christopher Boyce <57403492+chris-boyce@users.noreply.github.com>
2023-03-13 08:12:04 +0000

    File Writting and Network :( Im going to kill UE5

---
+++ Source/FYP5/Private/TextFileManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TextFileManager.h"
+#include "Misc/FileHelper.h"
+#include "HAL/PlatformFileManager.h"
+
+bool UTextFileManager::SaveArrayText(FString SaveDirectory, FString FileName, TArray<FString> SaveText,
+	bool AllowOverWriting = false)
+{
+	SaveDirectory += "\\";
+	SaveDirectory += FileName;
+	if(!AllowOverWriting)
+	{
+		if(FPlatformFileManager::Get().GetPlatformFile().FileExists(*SaveDirectory))
+		{
+			return false;
+		}
+	}
+	FString FinalString = "";
+	for (FString& Each : SaveText)
+	{
+		FinalString += Each;
+		FinalString += LINE_TERMINATOR;
+	}
+	return  FFileHelper::SaveStringToFile(FinalString, *SaveDirectory);
+}
